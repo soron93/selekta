4cbdc434c2fdf23462c70ed5d48fc55c372ccb96
emmyuxui <emmyuxui@gmail.com>
2021-07-21 22:31:45 +0200

    client side - failed attempt at Login with Spotify Button

--- src/components/TestEmmy.js
+++ src/components/TestEmmy.js
@@ -1,18 +1,83 @@
-import React, { Component } from 'react'
+//import React, { Component } from 'react'
 //import {Link} from 'react-router-dom'
 
-class TestEmmy extends Component {
+import React, { useEffect } from "react";
+//import SpotifyGetPlaylists from "./components/SpotifyGetPlaylists/SpotifyGetPlaylists";
+//import "./WebApp.css";
+
+
+const CLIENT_ID = "7ff64ba3320346b3be3b5977db49e2dd"; 
+const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
+const REDIRECT_URL_AFTER_LOGIN = "http://localhost:3000/";
+//const SPACE_DELIMITER = "%20";
+/*const SCOPES = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+  "playlist-read-private",
+];
+const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER); 
+
+DONT NEED THIS I THINK */
+
+/* 
+http://localhost:3000/webapp#access_token=ABCqxL4Y&token_type=Bearer&expires_in=3600
+*/
+const getReturnedParamsFromSpotifyAuth = (hash) => {
+  const stringAfterHashtag = hash.substring(1);
+  const paramsInUrl = stringAfterHashtag.split("&");
+  const paramsSplitUp = paramsInUrl.reduce((accumulater, currentValue) => {
+    console.log(currentValue);
+    const [key, value] = currentValue.split("=");
+    accumulater[key] = value;
+    return accumulater;
+  }, {});
+
+  return paramsSplitUp;
+};
+
+const TestEmmy = () => {
+  useEffect(() => {
+    if (window.location.hash) {
+      const { access_token, expires_in, token_type } =
+        getReturnedParamsFromSpotifyAuth(window.location.hash);
+
+      localStorage.clear();
+
+      localStorage.setItem("accessToken", access_token);
+      localStorage.setItem("tokenType", token_type);
+      localStorage.setItem("expiresIn", expires_in);
+    }
+  });
+
+  const handleLogin = () => {// took this out &scope=${SCOPES_URL_PARAM} after redirect URI login
+    window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&response_type=token&show_dialog=true`;
+  };
+
+  return (
+    <div className="container">
+      <h1>hi</h1>
+      <button onClick={handleLogin}>login to spotify</button>
+      
+    </div>
+  );
+};
+
+//export default SignUp;
+
+
+
+/*class TestEmmy extends Component {
 
     
     render() {
-      
+        const {spotify} = this.props
         
         return (
             <div>
-             testemmy
+             
             </div>
         )
     }
-}
+}*/
 
 export default TestEmmy
