ddde16c901657e6cc2a78cb4be62da0588b13fb6
emmyuxui <emmyuxui@gmail.com>
2021-07-21 21:35:18 +0200

    client added test pages and links, also in commponents & app.js

--- src/App.js
+++ src/App.js
@@ -12,11 +12,15 @@ import ChatBot from "./components/ChatBot";
 import './App.css'
 import  'bootstrap/dist/css/bootstrap.min.css';
 
+//TEST PAGES 
+import TestEmmy from './components/TestEmmy'
+import TestSal from './components/TestSal'
+
 
 class App extends Component {
 
   state = {
-    todos: [],
+    spotify: [],
     user: null,
     myError: null, 
     fetchingUser: true, 
@@ -26,10 +30,10 @@ class App extends Component {
   async componentDidMount(){
     try {
       // fetch all the initial todos to show on the home page
-        let response = await axios.get(`${API_URL}/api/todos`, {withCredentials: true})
+        let response = await axios.get(`${API_URL}/api/spotify`, {withCredentials: true})
         console.log(response.data)
         this.setState({
-          todos: response.data
+          spotify: response.data
         })
 
 
@@ -42,108 +46,13 @@ class App extends Component {
 
     }  
     catch(err){
-      console.log('Todo fetch failed', err)
+      console.log('Spotify fetch failed', err)
       this.setState({
         fetchingUser: false,
       })
     }
   }
 
-  handleAddTodo = async (event) => {
-
-    event.preventDefault()
-
-    //First upload the image to cloudinary
-    // then send the image url to our /api/create request
-    
-    // How to grab the image from our input 
-    console.log(event.target.myImage.files[0] )
-
-    let formData = new FormData()
-    formData.append('imageUrl', event.target.myImage.files[0])
-
-    let imgResponse = await axios.post(`${API_URL}/api/upload`, formData)
-    console.log(imgResponse)
-
-
-    let newTodo = {
-      name: event.target.name.value,
-      description: event.target.description.value,
-      completed: false,
-      image: imgResponse.data.image
-    }
-
-    // Pass the data in POST requests as the second parameter
-    // create the todo in the DB
-    axios.post(`${API_URL}/api/create`, newTodo, {withCredentials: true})
-      .then((response) => {
-          // Also update the state locally
-          // use the newly created to from your DB and not the local todo that we created above.
-
-          this.setState({
-            todos: [response.data, ...this.state.todos]
-          }, () => {
-              // to do something synchronous with the setState
-
-              // redirects the app to a certain url
-              // we're using the history push method to redirect it to any url we want
-              this.props.history.push('/')
-          })
-      })
-      .catch(() => {
-        console.log('Adding todo failed')
-      })
-
-  }
-
-  handleDeleteTodo = (todoId) => {
-    // delete the todo from the DB
-    axios.delete(`${API_URL}/api/todos/${todoId}`, {withCredentials: true})
-      .then(() => {
-        // and then also filter and remove the todo from the local state
-        let filteredTodos = this.state.todos.filter((todo) => {
-          return todo._id !== todoId
-        })
-
-        //update the state and redirect synchronously
-        this.setState({
-          todos: filteredTodos
-        } , () => {
-          this.props.history.push('/')
-        })
-
-      })
-      .catch(() => {
-        console.log('Delete failed')
-      })
-  }
-
-  handleEditTodo = (event, todo) => {
-    event.preventDefault()
-
-    // pass a second parameter to the patch for sending info to your server inside req.body
-    axios.patch(`${API_URL}/api/todos/${todo._id}`, todo, {withCredentials: true})
-      .then(() => {
-          // also update your local state here and redirect to home page
-          // mapping over all the todos and updating the one that was edited
-          let updatedTodos = this.state.todos.map((singleTodo) => {
-              if (singleTodo._id === todo._id) {
-                singleTodo.name = todo.name
-                singleTodo.description = todo.description
-              } 
-            return singleTodo
-          })
-
-          this.setState({
-            todos: updatedTodos
-          }, () => {
-             this.props.history.push('/')
-          })
-      })
-      .catch(() => {
-          console.log('Edit failed')
-      })
-  }
 
   handleSignUp = async (event) => {
     event.preventDefault()
@@ -249,6 +158,16 @@ class App extends Component {
               }}/>
               
               <Route component={NotFound} />
+
+              <Route  path="/testsal"  render={(routeProps) => {
+                return  <TestSal error={this.state.myError} {...routeProps}  />
+              }}/>
+
+              <Route  path="/testemmy"  render={(routeProps) => {
+                return  <TestEmmy error={this.state.myError} {...routeProps}  />
+              }}/>
+
+
           </Switch>
       </div>
     );
---
+++ src/components/TestEmmy.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+//import {Link} from 'react-router-dom'
+
+class TestEmmy extends Component {
+
+    
+    render() {
+      
+        
+        return (
+            <div>
+             testemmy
+            </div>
+        )
+    }
+}
+
+export default TestEmmy
